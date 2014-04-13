146245278bafbead72e9fccf2fea2c39a85f1855
cliffbar <cliff.bargar@gmail.com>
2014-04-13 15:49:14 -0700

    Get accelerometer data

--- Wheelchair_Main/Wheelchair_Main.ino
+++ Wheelchair_Main/Wheelchair_Main.ino
@@ -7,19 +7,30 @@
 
 //********** includes **********
 //Add the SPI library so we can communicate with the ADXL345 sensor
-#include <SPI.h>
-#include "ADXL345_consts.h"
+//#include <SPI.h>
+#include <Wire.h>
+#include "ADXL345.h"
+//wiring: http://www.inmotion.pt/documentation/others/images/adxl345_to_arduino_uno.png
+//SDA pin -> Analog 4 (Pull-up resistor to 3.3V)
+//SCL pin -> Analog 5 (Pull-up resistor to 3.3V)
+//CS pin -> 3.3V
+//VDD -> 3.3V
+//GND -> GND
 
 //********** defines **********
 #define CS 10 //Assign the Chip Select signal to pin 10.
 
-#define debugOn 1
-#define debugPrint if(debugOn) Serial.println
+#define DEBUG 1
+#define LEO 0
+#define debugPrint if(DEBUG) Serial.println
 
 //********** module variables **********
 //This buffer will hold values read from the ADXL345 registers.
-char values[10];
+int values[3];
 char output[20];
+
+int loopCount = 0;
+ADXL345 adxl; //variable adxl is an instance of the ADXL345 library
 
 //These variables will be used to hold the x,y and z axis accelerometer values.
 int x,y,z;
@@ -30,13 +41,21 @@ char tapType=0;
 //********** functions **********
 //Arduino setup()
 void setup(){ 
-  //Initiate an SPI communication instance.
-  SPI.begin();
-  //Configure the SPI connection for the ADXL345.
-  SPI.setDataMode(SPI_MODE3);
+  
+  pinMode(13,OUTPUT);
+  
   //Create a serial connection to display the data on the terminal.
   Serial.begin(9600);
   
+  //delay printing debug statements until Serial line opened
+  if(LEO & DEBUG)
+    while (!Serial)
+    {
+      digitalWrite(13,HIGH);
+      delay(50);
+      digitalWrite(13,LOW);
+      delay(50);
+    }
   debugPrint("ME 310 Stanford-Embraer Code Initialization Begun");
   
   //initialize accerelometer
@@ -45,18 +64,18 @@ void setup(){
 }
 
 //Arduino main loop()
-void loop(){
+void loop(){  
   //Reading 6 bytes of data starting at register DATAX0 will retrieve the x,y and z acceleration values from the ADXL345.
   //The results of the read operation will get stored to the values[] buffer.
-  readRegister(DATAX0, 6, values);
+  adxl.readAccel(&values[0], &values[1], &values[2]);
 
   //The ADXL345 gives 10-bit acceleration values, but they are stored as bytes (8-bits). To get the full value, two bytes must be combined for each axis.
   //The X value is stored in values[0] and values[1].
-  x = ((int)values[1]<<8)|(int)values[0];
+  x = values[0];
   //The Y value is stored in values[2] and values[3].
-  y = ((int)values[3]<<8)|(int)values[2];
+  y = values[1];
   //The Z value is stored in values[4] and values[5].
-  z = ((int)values[5]<<8)|(int)values[4];
+  z = values[2];
   
 //      Serial.println("SINGLE");
 //      Serial.print(x);
@@ -65,46 +84,19 @@ void loop(){
 //      Serial.print(',');
 //      Serial.println(z);
 
-  delay(10); 
-}
-
-//********** helper functions **********
-//This function will write a value to a register on the ADXL345.
-//Parameters:
-//  char registerAddress - The register to write a value to
-//  char value - The value to be written to the specified register.
-void writeRegister(char registerAddress, char value){
-  //Set Chip Select pin low to signal the beginning of an SPI packet.
-  digitalWrite(CS, LOW);
-  //Transfer the register address over SPI.
-  SPI.transfer(registerAddress);
-  //Transfer the desired register value over SPI.
-  SPI.transfer(value);
-  //Set the Chip Select pin high to signal the end of an SPI packet.
-  digitalWrite(CS, HIGH);
-}
-
-//This function will read a certain number of registers starting from a specified address and store their values in a buffer.
-//Parameters:
-//  char registerAddress - The register addresse to start the read sequence from.
-//  int numBytes - The number of registers that should be read.
-//  char * values - A pointer to a buffer where the results of the operation should be stored.
-void readRegister(char registerAddress, int numBytes, char * values){
-  //Since we're performing a read operation, the most significant bit of the register address should be set.
-  char address = 0x80 | registerAddress;
-  //If we're doing a multi-byte read, bit 6 needs to be set as well.
-  if(numBytes > 1)address = address | 0x40;
-  
-  //Set the Chip select pin low to start an SPI packet.
-  digitalWrite(CS, LOW);
-  //Transfer the starting register address that needs to be read.
-  SPI.transfer(address);
-  //Continue to read registers until we've read the number specified, storing the results to the input buffer.
-  for(int i=0; i<numBytes; i++){
-    values[i] = SPI.transfer(0x00);
+  if(loopCount == 100)
+  {
+   loopCount = 0; 
+      Serial.print(x, DEC);
+      Serial.print(',');
+      Serial.print(y, DEC);
+      Serial.print(',');
+      Serial.println(z, DEC);
   }
-  //Set the Chips Select pin high to end the SPI packet.
-  digitalWrite(CS, HIGH);
+
+
+  loopCount++;
+  delay(10); 
 }
 
 void initAccel()
@@ -114,36 +106,19 @@ void initAccel()
   //Before communication starts, the Chip Select pin needs to be set high.
   digitalWrite(CS, HIGH);
   
-  //Create an interrupt that will trigger when a tap is detected.
-  attachInterrupt(0, tap, RISING);
+  adxl.powerOn();
+  adxl.setRangeSetting(8); //16g range
+  //set rate to 3200Hz
+  adxl.setRate(1600); //sets rate bits of BW_RATE register to 0x0F
   
-  //Put the ADXL345 into +/- 4G range by writing the value 0x01 to the DATA_FORMAT register.
-  writeRegister(DATA_FORMAT, 0x01);
-
-  //Send the Tap and Double Tap Interrupts to INT1 pin
-  writeRegister(INT_MAP, 0x9F);
-  //Look for taps on the Z axis only.
-  writeRegister(TAP_AXES, 0x01);
-  //Set the Tap Threshold to 3g
-  writeRegister(THRESH_TAP, 0x38);
-  //Set the Tap Duration that must be reached
-  writeRegister(DURATION, 0x10);
   
-  //100ms Latency before the second tap can occur.
-  writeRegister(LATENT, 0x50);
-  writeRegister(WINDOW, 0xFF);
-  
-  //Enable the Single and Double Taps.
-  writeRegister(INT_ENABLE, 0xE0);  
-  
-  //Put the ADXL345 into Measurement Mode by writing 0x08 to the POWER_CTL register.
-  writeRegister(POWER_CTL, 0x08);  //Measurement mode
-  readRegister(INT_SOURCE, 1, values); //Clear the interrupts from the INT_SOURCE register.
+  debugPrint("Accelerometer Initialized");
 }
 
 void tap(void){
   //Clear the interrupts on the ADXL345
-  readRegister(INT_SOURCE, 1, values); 
+  //readRegister(INT_SOURCE, 1, values); 
   if(values[0] & (1<<5))tapType=2;
   else tapType=1;;
 }
+
