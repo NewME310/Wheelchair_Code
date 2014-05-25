b394611a802df90e30c20e85a74fe350c5945d8b
Cliff Bargar <cliff.bargar@gmail.com>
2014-05-25 02:36:47 -0700

    initial integration
    appears functional
    primary bugs:
    light comes on when shock is detected
    shock/FF detected together (likely wiring)

--- Wheelchair_Integrated/Wheelchair_Integrated.ino
+++ Wheelchair_Integrated/Wheelchair_Integrated.ino
@@ -2,22 +2,41 @@
 *  ME 310 - Team Embraer '13-'14
 *  Code for the Wheelchair-side system: integrated RFID and accelerometer
 *  Accelerometer code adapted from https://www.sparkfun.com/tutorials/240
+*  RFID code adapted from SparkFun code by Maria Barrera:
+  RFID Eval 13.56MHz Shield example sketch v10
+ 
+ Aaron Weiss, aaron at sparkfun dot com
+ OSHW license: http://freedomdefined.org/OSHW
+ 
+ works with 13.56MHz MiFare 1k tags 
 *  Cliff Bargar, 4/25/14
 */
+
+//wiring
+//Accelerometer http://www.inmotion.pt/documentation/others/images/adxl345_to_arduino_uno.png
+//SDA pin -> A4 (Uno) (Pull-up resistor to 3.3V)
+//SCL pin -> A5 (Uno) (Pull-up resistor to 3.3V)
+//CS pin -> 3.3V
+//VDD -> 3.3V
+//GND -> GND
+//RFID
+// Based on hardware v13:
+// D7 -> RFID RX
+// D8 -> RFID TX
+// D9 -> XBee TX
+// D10 -> XBee RX
+
 
 //********** includes **********
 //Add the SPI library so we can communicate with the ADXL345 sensor
 //#include <SPI.h>
 #include <Wire.h>
 #include "ADXL345.h"
-//wiring: http://www.inmotion.pt/documentation/others/images/adxl345_to_arduino_uno.png
-//SDA pin -> A4 (Uno) or 2 (Leo) (Pull-up resistor to 3.3V)
-//SCL pin -> A5 (Uno) or 3 (Leo) (Pull-up resistor to 3.3V)
-//CS pin -> 3.3V
-//VDD -> 3.3V
-//GND -> GND
+#include <SoftwareSerial.h>
+
 
 //********** defines **********
+//***Accel***
 //Free Fall threshold: 300m(g) to 600m(g) recommended
 #define FALL_THRESH 0x09 //recommended value                                    <-- needs to be adjusted
 //Free Fall duration: minimum time for free fall, 100ms to 350ms recommended
@@ -35,17 +54,22 @@
 #define FF_PIN 3 //interrupt for free fall on pin 3 (int.1) - INT1 on ADXL345
 #define FF_ISR 1
 
-//interrupt pins on Leonardo:
-//#define SHOCK_PIN 0 //interrupt for shock on pin 0 (int.2) - INT2 on ADXL345
-//#define SHOCK_ISR 2
-//#define FF_PIN 1 //interrupt for free fall on pin 1 (int.3) - INT1 on ADXL345
-//#define FF_ISR 3
-
 #define DEBUG 1
-#define LEO 1
 #define debugPrint if(DEBUG) Serial.println
 
+//***RFID***
+#define INDICATOR_PIN         13   //Pin for indicator LED
+#define INDICATOR_DURATION    700  //duration of indicator
+
+//last 2 digits of their serial number
+#define ALBERT 0x9A
+#define BOB 0xD0
+#define CHARLES 0x5A
+#define DIANA 0xC0
+#define ELTON 0x00
+
 //********** module variables **********
+//***Accel***
 //This buffer will hold values read from the ADXL345 registers.
 int values[3];
 char output[20];
@@ -61,6 +85,28 @@ char tapType=0;
 boolean shockFlag = 0;
 boolean fallFlag = 0;
 
+//***RFID***
+SoftwareSerial rfid(7, 8);
+SoftwareSerial xbee(10, 9);
+unsigned long indicator_time = -1;
+int flag = 0;
+int Str1[11];
+
+//Prototypes
+//***Accel***
+void initAccel(void);
+void tap(void);
+void ff_int_response(void);
+void shock_int_response(void);
+//***RFID***
+//void check_for_notag(void);
+void halt(void);
+void parse(void);
+void print_serial(void);
+void read_serial(void);
+void seek(void);
+void set_flag(void);
+
 //********** functions **********
 //Arduino setup()
 void setup(){ 
@@ -70,22 +116,23 @@ void setup(){
   //Create a serial connection to display the data on the terminal.
   Serial.begin(9600);
   
-  //delay printing debug statements until Serial line opened
-  if(LEO & DEBUG)
-    while (!Serial)
-    {
-      //flashes onboard LED while waiting for serial
-      digitalWrite(13,HIGH);
-      delay(50);
-      digitalWrite(13,LOW);
-      delay(50);
-    }
+  //RFID init
+  xbee.begin(9600);
+  rfid.begin(19200);
+  while(!Serial){
+    // set the data rate for the SoftwareSerial ports
+    ;
+  }
+  pinMode(INDICATOR_PIN,OUTPUT);
+  digitalWrite(INDICATOR_PIN,LOW);
+
+  delay(10);
+  //halt();
+  
   debugPrint("ME 310 Stanford-Embraer Code Initialization Begun");
   
   //initialize accerelometer
   initAccel();
-  
-  //initialize RFID
   
   //initialize XBee?
  
@@ -94,8 +141,14 @@ void setup(){
 //Arduino main loop()
 void loop(){  
   
+  //RFID
+  read_serial();
+  checkIndicator();
 
-//Reading data:  
+
+  //ACCEL
+  
+  //Reading data:  
   //Reading 6 bytes of data starting at register DATAX0 will retrieve the x,y and z acceleration values from the ADXL345.
   //The results of the read operation will get stored to the values[] buffer.
   adxl.readAccel(&values[0], &values[1], &values[2]);
@@ -133,6 +186,7 @@ void loop(){
   delay(100); 
 }
 
+//Accel helper functions
 void initAccel()
 {
    //Set up the Chip Select pin to be an output from the Arduino.
@@ -209,4 +263,147 @@ void shock_int_response()
     shockFlag = 1;  
 }
 
+//RFID helper functions
+void halt()
+{
+  //Halt tag
+  rfid.write((uint8_t)255);
+  rfid.write((uint8_t)0);
+  rfid.write((uint8_t)1);
+  rfid.write((uint8_t)147);
+  rfid.write((uint8_t)148);
+}
+
+void parse()
+{
+  //Serial.println("parse");
+  while(rfid.available()){
+    //Serial.println("available");
+    if(rfid.read() == 255){
+      for(int i=1;i<11;i++){
+        Str1[i]= rfid.read();
+      }
+    }
+  }
+}
+
+void print_serial()
+{
+  if(flag == 1){
+    indicatorOn();
+    //print to serial port
+//    Serial.print(Str1[8], HEX);
+//    Serial.print(Str1[7], HEX);
+//    Serial.print(Str1[6], HEX);
+//    Serial.print(Str1[5], HEX);
+//    Serial.println();
+    //tagid=Str1[8]+Str1[7]+Str1[6]+Str1[5]
+//    print_handler();
+ 
+    //print to XBee module
+    xbee.print(Str1[8], HEX);
+    xbee.print(Str1[7], HEX);
+    xbee.print(Str1[6], HEX);
+    xbee.print(Str1[5], HEX);
+    xbee.println();
+    delay(100);
+    //check_for_notag();
+  }
+}
+
+void print_handler()
+{
+  if (Str1[5]==ALBERT)
+  {
+    Serial.println("Albert is now handling your wheelchair");
+  } 
+  else if (Str1[5]==BOB)
+  {
+    Serial.println("Bob is now handling your wheelchair");
+  } 
+  else if (Str1[5]==CHARLES)
+  {
+    Serial.println("Charles is now handling your wheelchair");
+  } 
+  else if (Str1[5]==DIANA)
+  {
+    Serial.println("Diana is now handling your wheelchair");
+  } 
+  else if (Str1[5]==ELTON)
+  {
+    Serial.println("Elton is now handling your wheelchair");
+  } 
+  else 
+  {
+    //do nothing?
+  }
+  //delay(1000); //so there arent repeat alerts
+    
+//  switch (Str1[8], HEX) {
+//  case ALBERT:
+//    Serial.println("Albert is now handling your wheelchair");
+//    break;
+//  case BOB:
+//    Serial.println("Bob is now handling your wheelchair");
+//    break;
+//  case CHARLES:
+//    Serial.println("Charles is now handling your wheelchair");
+//    break;
+//  case DIANA:
+//    Serial.println("Diana is now handling your wheelchair");
+//    break;
+//  case ELTON:
+//    Serial.println("Elton is now handling your wheelchair");
+////    break;
+  
+}
+
+
+
+void read_serial()
+{
+  seek();
+  delay(10);
+  parse();
+  set_flag();
+  print_serial();
+  delay(100);
+}
+
+void seek()
+{
+  //search for RFID tag
+
+  //Serial.println("seeking");
+
+  rfid.write((uint8_t)255);
+  rfid.write((uint8_t)0);
+  rfid.write((uint8_t)1);
+  rfid.write((uint8_t)130);
+  rfid.write((uint8_t)131);
+  delay(10);
+}
+
+void set_flag()
+{
+  if(Str1[2] == 6){
+    flag++;
+  }
+  if(Str1[2] == 2){
+    flag = 0;
+  }
+}
+
+void indicatorOn()
+{
+  indicator_time = millis();
+  digitalWrite(INDICATOR_PIN,HIGH);
+}
+
+//determine if indicator light should be turned off
+void checkIndicator()
+{
+  if((unsigned long)(millis() - indicator_time) > INDICATOR_DURATION && indicator_time != -1)
+    digitalWrite(INDICATOR_PIN,LOW);
+}
 
